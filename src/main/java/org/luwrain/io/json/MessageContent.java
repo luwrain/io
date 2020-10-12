9ed7d72298acfac0f06c28579cae00d0362936c7
Michael Pozhidaev <msp@luwrain.org>
2020-10-12 08:44:43 +0700

    Adding json.MessageContent

---
+++ src/main/java/org/luwrain/io/json/MessageContent.java
@@ -0,0 +1,81 @@
+/*
+   Copyright 2012-2020 Michael Pozhidaev <msp@luwrain.org>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+package org.luwrain.app.message;
+
+import java.util.*;
+import com.google.gson.*;
+import com.google.gson.annotations.*;
+
+import org.luwrain.core.*;
+
+public final class MessageContent
+{
+    @SerializedName("to")
+    public String to = "";
+
+    @SerializedName("cc")
+    public String cc = "";
+
+    @SerializedName("subject")
+    public String subject = "";
+
+    @SerializedName("text")
+    public List<String> text;
+
+    @SerializedName("attachments")
+    public List<String> attachments = null;
+
+    public MessageContent(String to, String cc, String subject, String[] text)
+    {
+	NullCheck.notNull(to, "to");
+	NullCheck.notNull(cc, "cc");
+	NullCheck.notNull(subject, "subject");
+	NullCheck.notNullItems(text, "text");
+	this.to = to;
+	this.cc = cc;
+	this.subject = subject;
+	this.text = Arrays.asList(text);
+	this.attachments = Arrays.asList(new String[0]);
+    }
+
+    public String[] getTextAsArray()
+    {
+	if (text == null)
+	    return new String[0];
+	return text.toArray(new String[text.size()]);
+    }
+
+    public String[] getAttachmentsAsArray()
+    {
+	if (attachments == null)
+	    return new String[0];
+	return attachments.toArray(new String[attachments.size()]);
+    }
+
+    public String toJson()
+    {
+	return new Gson().toJson(this);
+    }
+
+    static public MessageContent fromJson(String text)
+    {
+	NullCheck.notNull(text, "text");
+	if (text.isEmpty())
+	    return null;
+	return new Gson().fromJson(text, MessageContent.class);
+    }
+}
