d7157ac0b41159a42debfd54a21dbf1743907ce1
Jl4cTuk <jl4ctuk@gmail.com>
2024-06-03 15:35:33 +0700

    fixed Searx.java + add Gson

--- src/main/java/org/luwrain/io/api/searx/Searx.java
+++ src/main/java/org/luwrain/io/api/searx/Searx.java
@@ -2,6 +2,8 @@ package org.luwrain.io.api.searx;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -11,8 +13,8 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.apache.logging.log4j.*;
 
 import java.io.IOException;
@@ -23,15 +25,24 @@ import java.util.List;
 
 public class Searx {
 
-    private static final Logger logger = LogManager.getLogger();
+    private static final Logger logger = LoggerFactory.getLogger(Searx.class);
+    private static final Gson gson = new Gson();
 
     public static void main(String[] args) {
-        String query = "coffee"; // поисковой запрос
-        String requestMethod = "POST"; // метод
-        String searxngUrl = "https://searx.sev.monster/search"; // URL searxng клиента
+        String query = "coffee";
+        String requestMethod = "POST";
+        String[] searxngUrls = {
+                "http://localhost:8080/search", // локальный searxng
+                "https://search.mdosch.de/search",
+                "https://search.projectsegfau.lt/search",
+                "https://search.us.projectsegfau.lt/search",
+                "https://searx.sev.monster/search",
+                "https://etsi.me/search",
+                "https://search.in.projectsegfau.lt/search"
+        };
+        String searxngUrl = searxngUrls[0];
         String payload = "q=" + URLEncoder.encode(query, StandardCharsets.UTF_8) + "&format=json";
 
-        // заголовки которые я добавил чтобы копия из интернета работала
         String[] headers = {
                 "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0",
                 "Accept: */*",
@@ -41,12 +52,20 @@ public class Searx {
                 "Content-Type: application/x-www-form-urlencoded"
         };
 
+        List<String> links = sendRequest(searxngUrl, requestMethod, payload, headers);
+        String json = gson.toJson(links);
+        printLinksAsJson(json);
+    }
+
+    private static List<String> sendRequest(String url, String method, String payload, String[] headers) {
+        List<String> links = new ArrayList<>();
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpRequestBase request = createRequest(requestMethod, searxngUrl, payload, headers);
-            executeRequest(httpClient, request);
+            HttpRequestBase request = createRequest(method, url, payload, headers);
+            links = executeRequest(httpClient, request);
         } catch (IOException e) {
             logger.error("Error occurred while making HTTP request", e);
         }
+        return links;
     }
 
     private static HttpRequestBase createRequest(String method, String url, String payload, String[] headers) throws IOException {
@@ -68,24 +87,27 @@ public class Searx {
         return request;
     }
 
-    private static void executeRequest(CloseableHttpClient httpClient, HttpRequestBase request) throws IOException {
+    private static List<String> executeRequest(CloseableHttpClient httpClient, HttpRequestBase request) throws IOException {
+        List<String> links = new ArrayList<>();
         try (CloseableHttpResponse response = httpClient.execute(request)) {
-            handleResponse(response);
+            links = handleResponse(response);
         }
+        return links;
     }
 
-    private static void handleResponse(CloseableHttpResponse response) throws IOException {
+    private static List<String> handleResponse(CloseableHttpResponse response) throws IOException {
+        List<String> links = new ArrayList<>();
         HttpEntity entity = response.getEntity();
         if (entity != null) {
             String responseString = EntityUtils.toString(entity);
             if (response.getStatusLine().getStatusCode() == 200 && isJson(responseString)) {
-                List<String> links = parseAndPrintLinks(responseString);
-                printLinks(links);
+                links = parseAndPrintLinks(responseString);
             } else {
                 logger.error("Received non-JSON response or error code: " + response.getStatusLine());
                 logger.error("Response: " + responseString);
             }
         }
+        return links;
     }
 
     private static boolean isJson(String responseString) {
@@ -111,9 +133,7 @@ public class Searx {
         return links;
     }
 
-    private static void printLinks(List<String> links) {
-        for (String link : links) {
-            logger.info(link);
-        }
+    private static void printLinksAsJson(String json) {
+        logger.info(json);
     }
 }
